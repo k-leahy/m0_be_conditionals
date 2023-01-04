fbc3ae3ba6602a18f03db242042654d387f4c4a8
Kelly <119141863+k-leahy@users.noreply.github.com>
2023-01-04 13:32:01 -0700

    Insert pt3

--- comparisons.rb
+++ comparisons.rb
@@ -89,15 +89,15 @@ age = 1
 
 # YOU DO:
 # Determine if the dog loves to play and loves treats
-
+puts loves_to_play && loves_treats
 
 # Determine if the dog loves to play and loves the dog park
-
+puts loves_to_play && loves_dog_park
 
 # Determine if the dog loves to play or loves the dog park
-
+puts loves_to_play || loves_dog_park
 
 # Determine if the dog loves to play and is a puppy
-
+puts loves_to_play && age < 1
 # What did your final line of code evaluate to? Why do you think that is? Explain.
 # ANSWER:
