62827b38376973e17bdc79560c54c32957987019
Kelly <119141863+k-leahy@users.noreply.github.com>
2023-01-04 14:59:59 -0700

    Create code

---
+++ code_challenges/challenge_medium.rb
@@ -0,0 +1,11 @@
+good_driving_record = true
+age = 24
+
+if good_driving_record == true && age>25
+    puts "Get discount pm car rental"
+    elsif good_driving_record == false && age < 25
+        "Need a cosigner"
+    else
+        "Pay full price"
+    end
+    
