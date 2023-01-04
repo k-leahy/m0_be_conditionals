b74a74db96f93d06ee7cae1f686543a0d142a471
Kelly <119141863+k-leahy@users.noreply.github.com>
2023-01-04 13:57:04 -0700

    Change conditionals

--- if_statements.rb
+++ if_statements.rb
@@ -10,7 +10,7 @@
   # if it is snowy, print "coat"
   # if it is icy, print "yak traks"
 
-weather = "sunny"
+weather = "else"
 
 if weather == "sunny"
   p "sunscreen"
@@ -41,10 +41,13 @@ end
 # Experiment with manipulating the value held within num_quarters
 # to make sure both conditions can be achieved.
 
-num_quarters = 0
+num_quarters = 6
 
+if num_quarters >= 2
 puts "I have enough money for a gumball"
+elsif num_quarters < 2
 puts "I don't have enough money for a gumball"
+end
 
 
 #####################
@@ -61,5 +64,13 @@ puts "I don't have enough money for a gumball"
 # Experiment with manipulating the value held within both variables
 # to make sure all above conditions output what you expect.
 
-cups_of_flour = 1
-has_sauce = true
+cups_of_flour = 6
+has_sauce = false
+
+if has_sauce == false
+  print "I cannot make pizza"
+elsif cups_of_flour < 2
+  print "I cannot make pizza"
+else 
+  print "I can make pizza"
+end
